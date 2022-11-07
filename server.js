73567a3207a0d7b669f9f1fe1872991a67511aab
nickcoimbra <nicolascoimbra2@gmail.com>
2022-11-06 21:35:39 -0300

    fix views from first challenge

--- server.js
+++ server.js
@@ -12,6 +12,7 @@ app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
 app.set('view engine', 'pug')
+app.set('views', './views/pug')
 
 app.route('/').get((req, res) => {
   res.render('pug')
